f0c543c39c33f642e99af38e732dcb27988a3d96
piyushdevcode <17671660+piyushdevcode@users.noreply.github.com>
2023-04-26 12:26:07 +0530

    pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = 8080;
 
-app.get("/", (req, res) => res.send("Hello scooby doo!"));
+app.get("/", (req, res) => res.send("Hola Amigos !"));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
